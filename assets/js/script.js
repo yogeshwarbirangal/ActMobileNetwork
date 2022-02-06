27ed3730be221337d51c49bf3e3e054e8dbe2a91
Yogeshwar <yogeshwarbirangal@gmail.com>
2022-02-06 13:51:48 +0530

    code updated

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+        $('.testimonials').slick({
+              infinite: true,
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              autoplay: true,
+              autoplaySpeed: 2000,
+              arrows:false,
+              responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+ })    
+
+
+ $(document).ready(function(){
+        $("#myModal").modal('show');
+    });
+
+
+    // greeting js
+
+        const greeting = document.getElementById("greeting");
+        const date = new Date();
+        const hour = date.getHours();
+        console.log(hour);
+
+        const textType = ["Good Morning!", "Good Afternoon!", "Good Evening!", "Good Night!"];
+        let welcomeText = '';
+
+
+            if(hour >= 0 && hour < 12){
+                welcomeText = textType[0];
+            }
+            else if( hour >=12 && hour <= 18 ){
+                
+                welcomeText = textType[1];
+            }
+            else if ( hour > 18 && hour < 22){
+                welcomeText = textType[2];
+            }
+            else if( hour >= 22 && hour > 0){
+                welcomeText = textType[3]
+            }
+            greeting.innerHTML = welcomeText
+
+
+            setTimeout(() => {
+
+              $('#myModal').modal('hide');
+                        
+            }, 10000); 
+
+            // background color change
+
+            function changeBackground(){
+
+
+
+             let color = document.getElementById('colorInputText').value;
+                
+                if(color.length <= 7)
+                {
+                      document.body.style.backgroundColor =  color;
+                }
+
+        }
